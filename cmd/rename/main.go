f8bc1093b16610547472033fd350bb5eec957a1c
Mmx <green.dusk8264@fastmail.com>
2024-10-07 22:04:32 +0800

    feat: allow custom regexp

--- cmd/rename/main.go
+++ cmd/rename/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Mmx233/EpRename/match"
 	"os"
 	"path/filepath"
+	"regexp"
 
 	nested "github.com/antonfisher/nested-logrus-formatter"
 	log "github.com/sirupsen/logrus"
@@ -32,10 +33,25 @@ func main() {
 		log.Fatal(err)
 	}
 
+	var _GetEpisodeSerial func(string) (string, error)
+	if len(os.Args) == 1 {
+		_GetEpisodeSerial = match.GetEpisodeSerial
+	} else {
+		customRegexp := os.Args[1]
+		reg, err := regexp.Compile(customRegexp)
+		if err != nil {
+			log.Fatalf("compile custom regexp '%s' failed: %v", customRegexp, err)
+		}
+		log.Infof("using custom regexp: %s", customRegexp)
+		_GetEpisodeSerial = func(filename string) (string, error) {
+			return match.GetEpisodeSerialWithRegexp(reg, filename)
+		}
+	}
+
 	for _, file := range dir {
 		fileDir, fileName := filepath.Split(file.Name())
 		ext := filepath.Ext(fileName)
-		episode, err := match.GetEpisodeSerial(fileName)
+		episode, err := _GetEpisodeSerial(fileName)
 		if err != nil {
 			log.Warnf("rename '%s' failed: %v", fileName, err)
 			continue
--- match/episode.go
+++ match/episode.go
@@ -13,3 +13,11 @@ func GetEpisodeSerial(filename string) (string, error) {
 	}
 	return match[2], nil
 }
+
+func GetEpisodeSerialWithRegexp(reg *regexp.Regexp, filename string) (string, error) {
+	match := reg.FindStringSubmatch(filename)
+	if len(match) == 0 {
+		return "", fmt.Errorf("match episode serial failed: %s", filename)
+	}
+	return match[1], nil
+}
