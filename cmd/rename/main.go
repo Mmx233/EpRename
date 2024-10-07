f5e54f15c15217c69032cd14cb255a01f796b763
Mmx <green.dusk8264@fastmail.com>
2024-10-07 21:49:56 +0800

    improve: logs

--- cmd/rename/main.go
+++ cmd/rename/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/Mmx233/EpRename/match"
-	"log"
 	"os"
 	"path/filepath"
+
+	nested "github.com/antonfisher/nested-logrus-formatter"
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+	log.SetFormatter(&nested.Formatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+}
 
 func main() {
 	pwd, err := os.Getwd()
@@ -29,16 +37,18 @@ func main() {
 		ext := filepath.Ext(fileName)
 		episode, err := match.GetEpisodeSerial(fileName)
 		if err != nil {
-			log.Fatal(err)
+			log.Warnf("rename '%s' failed: %v", fileName, err)
+			continue
 		}
+		episodeName := fmt.Sprintf("S%sE%s%s", season, episode, ext)
 		if err = os.Rename(
 			file.Name(),
-			filepath.Join(
-				fileDir,
-				fmt.Sprintf("S%sE%s%s", season, episode, ext),
-			),
+			filepath.Join(fileDir, episodeName),
 		); err != nil {
-			log.Fatal(err)
+			log.Errorf("rename '%s' failed: %v", fileName, err)
+			continue
 		}
+
+		log.Infof("successfully renamed '%s' to '%s'", fileName, episodeName)
 	}
 }
